7c47906b457813651ff3e84c29f36740260a89a1
MaryKaratu <Karatumary85@gmail.com>
2025-06-06 15:36:33 +0100

    When Not to lift State Up

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,11 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2",
+  });
+
   const [gameTurns, setGameTurns] = useState([]);
   // const [hasWinner, setHasWinner] = useState(false);Not needed
   // const [activePlayer, setActivePlayer] = useState("X");
@@ -82,6 +87,15 @@ function App() {
 
   function handleRestart() {
     setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
   }
 
   return (
